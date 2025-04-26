05a171bba61101e4a4847c11da7da7a3b1476fdc
Phúc Trần <tranbaophuc2703@gmail.com>
2025-04-26 10:09:06 +0700

    add docker into traffic_service, having error when using localhost

--- backend/traffic_service/connection.py
+++ backend/traffic_service/connection.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 # Connect MongoDB using environment variable for security
-MONGO_URI = os.getenv("mongodb://192.168.137.101:27017/")
+MONGO_URI = os.getenv("localhost:27017/")
 client = MongoClient(MONGO_URI)
 
 # Choose database and collection
