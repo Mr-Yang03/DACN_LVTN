d801a7c07a89973d0ae092caf58a603b755ee499
SThanhf <thanh.nguyensinh012@hcmut.edu.vn>
2025-05-11 02:53:59 +0700

    create user board api

---
+++ frontend/src/apis/userboardApi.ts
@@ -0,0 +1,26 @@
+
+import { api } from "./axiosInstance";
+
+const PREFIX = "/userboard/ub";
+
+export const getUsers = () => api.get(PREFIX);
+
+export const getUser = (id: string) => api.get(`${PREFIX}/${id}`);
+
+export const createUser = (user: {
+  full_name: string;
+  date_of_birth: string;
+  phone_number: string;
+  license_number: string;
+  account_id?: string;
+}) => api.post(PREFIX, user);
+
+export const updateUser = (id: string, user: {
+  full_name: string;
+  date_of_birth: string;
+  phone_number: string;
+  license_number: string;
+  account_id?: string;
+}) => api.put(`${PREFIX}/${id}`, user);
+
+export const deleteUser = (id: string) => api.delete(`${PREFIX}/${id}`);
